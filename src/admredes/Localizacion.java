19ddd0255fac91f742abcb33b20c9556e3140f90
Ricardo R. Ibarra <Ricardo R. Ibarra@LapTop-Richz>
2024-02-15 01:54:28 -0600

    Problemas en la actualización pasada, se corrigió.

--- src/admredes/Localizacion.java
+++ src/admredes/Localizacion.java
@@ -4,6 +4,7 @@
  */
 package admredes;
 
+import java.awt.Color;
 import java.awt.Image;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -17,15 +18,15 @@ public class Localizacion extends javax.swing.JFrame {
     /**
      * Creates new form Localizacion
      */
-    private ImageIcon imagen;
-    private Icon icono;
-    
+    saladecomputo1 loca = new saladecomputo1();
 
     public Localizacion() {
         initComponents();
-        this.setLocationRelativeTo(this);
-        this.pintarImagen(this.Plano1, "src/imagenes/Plano2.png");
-         
+        JPanelImage mImage = new JPanelImage(Croquis1, "src/imagenes/plano2.png");
+        Croquis1.add(mImage).repaint();
+        Croquis1.setOpaque(false);
+        Croquis1.setBorder(null);
+        Croquis1.setBackground(new  Color(0,0,0,0));
     }
 
     /**
@@ -37,22 +38,30 @@ public class Localizacion extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        Plano1 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        Croquis1 = new javax.swing.JPanel();
+        SC1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        Plano1.setBackground(new java.awt.Color(0, 0, 0));
-        Plano1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
         jLabel1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jLabel1.setText("Localización de dispositivos");
 
-        jButton1.setText("Regresar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        javax.swing.GroupLayout Croquis1Layout = new javax.swing.GroupLayout(Croquis1);
+        Croquis1.setLayout(Croquis1Layout);
+        Croquis1Layout.setHorizontalGroup(
+            Croquis1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 983, Short.MAX_VALUE)
+        );
+        Croquis1Layout.setVerticalGroup(
+            Croquis1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 539, Short.MAX_VALUE)
+        );
+
+        SC1.setText("Sala C. 1");
+        SC1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                SC1ActionPerformed(evt);
             }
         });
 
@@ -61,40 +70,39 @@ public class Localizacion extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(25, 25, 25)
-                .addComponent(Plano1, javax.swing.GroupLayout.PREFERRED_SIZE, 741, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(56, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jLabel1)
                         .addGap(269, 269, 269))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jButton1)
-                        .addContainerGap())))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(SC1)
+                        .addGap(921, 921, 921))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(Croquis1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
+            .addGroup(layout.createSequentialGroup()
+                .addGap(9, 9, 9)
                 .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
-                .addComponent(Plano1, javax.swing.GroupLayout.PREFERRED_SIZE, 560, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jButton1)
-                .addGap(10, 10, 10))
+                .addComponent(SC1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(Croquis1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void SC1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SC1ActionPerformed
         // TODO add your handling code here:
-        Menu men = new Menu();
-        men.setVisible(true);
-        this.setVisible(false);
-    }//GEN-LAST:event_jButton1ActionPerformed
+        loca.setVisible(true);
+    }//GEN-LAST:event_SC1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -130,23 +138,10 @@ public class Localizacion extends javax.swing.JFrame {
             }
         });
     }
-
-    private void pintarImagen(JLabel img, String ruta){
-        this.imagen = new ImageIcon(ruta);
-        this.icono = new ImageIcon(
-                this.imagen.getImage().getScaledInstance(
-                        img.getWidth(), 
-                        img.getHeight(), 
-                        Image.SCALE_DEFAULT
-                )
-        );
-        img.setIcon(this.icono);
-        this.repaint();
-    }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel Plano1;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel Croquis1;
+    private javax.swing.JButton SC1;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
